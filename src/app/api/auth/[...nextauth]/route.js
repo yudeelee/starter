e3322cdf27925cfa1c4d7a2a76566056b80659b6
Yura Dmytruk <study.on.work@gmail.com>
2023-10-12 19:55:14 +0300

    first

---
+++ src/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,52 @@
+import NextAuth from 'next-auth/next';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import { MongoDBAdapter } from '@auth/mongodb-adapter';
+import clientPromise from './lib/mongodb';
+import db from '../../../utils/db.js';
+import User from '@/models/User';
+import bcrypt from 'bcrypt';
+
+db.connectDb();
+
+export const authOptions = {
+  adapter: MongoDBAdapter(clientPromise),
+  providers: [
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        username: { label: 'Username', type: 'text', placeholder: 'jsmith' },
+        password: { label: 'Password', type: 'password' },
+      },
+      async authorize(credentials, req) {
+        const email = credentials.email;
+        const password = credentials.password;
+        const user = await User.findOne({ email });
+        if (user) {
+          return SignInUser({ password, user });
+        } else {
+          throw new Error('This email does not exist.');
+        }
+      },
+    }),
+  ],
+  pages: {
+    signIn: '/admin',
+  },
+  session: {
+    strategy: 'jwt',
+  },
+  secret: process.env.JWT_SECRET,
+};
+
+export default NextAuth(authOptions);
+
+const SignInUser = async ({ password, user }) => {
+  if (!user.password) {
+    throw new Error('Please enter your password.');
+  }
+  const testPassword = await bcrypt.compare(password, user.password);
+  if (!testPassword) {
+    throw new Error('Email or password is wrong!');
+  }
+  return user;
+};
